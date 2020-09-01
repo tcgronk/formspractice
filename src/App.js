b551562a804eaee1681472e3f3d5767efb5dd1d7
Tess Gronski <tessgronski@gmail.com>
2020-08-31 22:14:34 -0700

    Adds job title & validation

--- src/App.js
+++ src/App.js
@@ -5,46 +5,67 @@ class App extends React.Component {
   state = {
     fName: "",
     lName: "",
+    jobTitle: "",
+    error: "",
   };
+
   handleInputChange = ({ target }) => {
-    this.setState({ [target.name]: target.value });
+    this.setState({ [target.name]: target.value, error: "" });
   };
   handleFormSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state);
-    //API call here
-
-    this.setState({
-      fName: "",
-      lName: "",
-    });
+    if (!this.state.jobTitle) {
+      this.setState({ error: "Please select a job title" });
+    } else
+      // Api call
+      this.setState({
+        fName: "",
+        lName: "",
+        jobTitle: "",
+        error: "",
+      });
   };
   render() {
     return (
       <div className="App">
         <form>
-            <h1>Form</h1>
-            <p>
-              <label htmlFor="first-name">First Name: </label>
-              <input
-                type="text"
-                id="first-name"
-                name="fName"
-                value={this.state.fName}
-                onChange={(e) => this.handleInputChange(e)}
-              />
-            </p>
-            <p>
-              <label htmlFor="last-name">Last Name: </label>
-              <input
-                type="text"
-                id="last-name"
-                name="lName"
-                value={this.state.lName}
-                onChange={(e) => this.handleInputChange(e)}
-              />
-            </p>
+          <h1>Form</h1>
+          <p>
+            <label htmlFor="first-name">First Name: </label>
+            <input
+              type="text"
+              id="first-name"
+              name="fName"
+              value={this.state.fName}
+              onChange={(e) => this.handleInputChange(e)}
+            />
+          </p>
+          <p>
+            <label htmlFor="last-name">Last Name: </label>
+            <input
+              type="text"
+              id="last-name"
+              name="lName"
+              value={this.state.lName}
+              onChange={(e) => this.handleInputChange(e)}
+            />
+          </p>
+          <p>
+            <select
+              name="jobTitle"
+              value={this.state.jobTitle}
+              onChange={(e) => this.handleInputChange(e)}
+            >
+              <option value={""}>
+                --Select One--
+              </option>
+              <option value="Software Engineer">Software Engineer</option>
+              <option value="Data Engineer">Data Engineer</option>
+              <option value="Security Engineer">Security Engineer</option>
+            </select>
+          </p>
           <button onClick={(e) => this.handleFormSubmit(e)}>Submit</button>
+          <h3>{this.state.error}</h3>
         </form>
       </div>
     );
